ceaec637a5969ad4f8975ea73190088106411588
Tyrone <tyrone.cloma0219@gmail.com>
2022-09-06 14:50:08 -0400

    solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+export function createStore(reducer) {
+  let state;
+
+  const getState = () => {
+    return state;
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    render()
+    return state;
+  }
+
+  return { getState, dispatch }
 }
 
 export function candyReducer(state = [], action) {
@@ -24,3 +36,6 @@ function render() {
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+
+const store = createStore(candyReducer)
+store.dispatch({ type: '@@INIT' })
